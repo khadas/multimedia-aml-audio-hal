1b18a1038cc9862193bae208ab5d480bc4dc0eb5
Tim Yao <tim.yao@amlogic.com>
2021-01-06 23:43:37 -0800

    audio_hal: audio_cap muting for patch input. [1/1]
    PD#TV-29719
    
    Problem:
    Audio cap is defined to be muted for patch source.
    
    Solution:
    Clear audio cap data for patch source.
    
    Verify:
    Verified on T5D with Roku APP and TV playing from
    HDMI IN.
    
    Change-Id: I6bb069fdfc9ce59a06722c1a2afa307beeb70d14

--- audio_hal/audio_hw.c
+++ audio_hal/audio_hw.c
@@ -8323,6 +8323,10 @@ ssize_t audio_hal_data_processing(struct audio_stream_out *stream,
                     /* 2ch downmix capture */
                     pthread_mutex_lock(&adev->cap_buffer_lock);
                     if (adev->cap_buffer) {
+                        if (adev->audio_patch) {
+                            /* capture only works for non-patch case by definition */
+                            memset(tmp_buffer, 0, out_frames * 4);
+                        }
                         IpcBuffer_write(adev->cap_buffer, (const unsigned char *)tmp_buffer, out_frames * 4); 
                     }
                     pthread_mutex_unlock(&adev->cap_buffer_lock);
