51b4c239192b2d4cf15f07ac6dba7411ffe33bbe
Tim Yao <tim.yao@amlogic.com>
2021-03-26 22:47:20 -0700

    audiohal: Improve APTS reset PCR when underrun. v2. [1/1]
    PD#TV-31636
    
    Problem:
    The previous change 52e589f7ce9b134d6d690604ab3a2ff7d8d731bf
    has a wrong condition set to make amaster apts->pcr reset not
    working when audio is lagging behind video.
    
    Solution:
    Fix the condition.
    
    Verify:
    Verify PCR reset when audio PTS is behind PCR.
    
    Change-Id: Ife64e3e1a95d300e20c17e7fb2a2f36358c02492

--- audio_hal/audio_hwsync.c
+++ audio_hal/audio_hwsync.c
@@ -521,18 +521,18 @@ int aml_audio_hwsync_audio_process(audio_hwsync_t *p_hwsync, size_t offset, int 
                         ALOGE("pcr has been reset\n");
                     }
                 } else {
-                    if (adev->continuous_audio_mode && eDolbyMS12Lib == adev->dolby_lib_type) {
+                    if (adev->continuous_audio_mode &&
+                        eDolbyMS12Lib == adev->dolby_lib_type &&
+                        (adev->ms12.underrun_cnt != dolby_ms12_get_main_underrun())) {
                         /* when MS12 main pipeline gets underrun, the output latency does not reflect the real
                          * latency since muting frame can be inserted by MS12 when continuous output mode is
                          * enabled, so only reset APTS when PCR exceeds last checkin PTS in such case.
                          */
-                        if (adev->ms12.underrun_cnt != dolby_ms12_get_main_underrun()) {
-                            gap = pts_gap(pcr, apts_save);
-                            if (pts_bigger(pcr, apts_save)) {
-                                sprintf(tempbuf, "0x%x", apts_save);
-                                sysfs_set_sysfs_str(TSYNC_APTS, tempbuf);
-                                ALOGI("tsync -> reset pcrscr 0x%x -> 0x%x", pcr, apts_save);
-                            }
+                        gap = pts_gap(pcr, apts_save);
+                        if (pts_bigger(pcr, apts_save)) {
+                            sprintf(tempbuf, "0x%x", apts_save);
+                            sysfs_set_sysfs_str(TSYNC_APTS, tempbuf);
+                            ALOGI("tsync -> reset pcrscr 0x%x -> 0x%x", pcr, apts_save);
                         }
                     } else {
                         ALOGI("tsync -> reset pcrscr 0x%x -> 0x%x, %s big,diff %"PRIx64" ms",
