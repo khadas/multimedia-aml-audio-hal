cbe7c5fa5c342aa6c66b7cccd2a7e06258e8f714
yuliang.hu <yuliang.hu@amlogic.com>
2023-02-20 14:48:02 +0800

    audio: Adjust alsa open configs in audiohal [1/1]
    PD#SWPL-111897
    
    Problem:
    No audio when play ddp/dd/dts format with dts or ddp license.
    
    Solution:
    Adjust alsa open configs, software buffer less than hardware buffer.
    
    Verify:
    yocto-t5w.
    
    Change-Id: I585ad5a751b3e226c5cc522c01d3c6ff2af166eb
    Signed-off-by: yuliang.hu <yuliang.hu@amlogic.com>

--- audio_hal/audio_hw.c
+++ audio_hal/audio_hw.c
@@ -214,8 +214,8 @@ static const struct pcm_config pcm_config_out = {
 static const struct pcm_config pcm_config_out_direct = {
     .channels = 2,
     .rate = MM_FULL_POWER_SAMPLING_RATE,
-    .period_size = DEFAULT_PLAYBACK_PERIOD_SIZE * 2,
-    .period_count = PLAYBACK_PERIOD_COUNT * 2,
+    .period_size = DEFAULT_PLAYBACK_PERIOD_SIZE,
+    .period_count = PLAYBACK_PERIOD_COUNT,
     .format = PCM_FORMAT_S16_LE,
 };
 
@@ -3293,7 +3293,7 @@ static int adev_open_output_stream(struct audio_hw_device *dev,
         }
 
         out->config = pcm_config_out_direct;
-        out->config.start_threshold = out->config.period_size * 2;
+        out->config.start_threshold = out->config.period_size * 4;
         out->config.channels = audio_channel_count_from_out_mask(config->channel_mask);
         out->config.rate = config->sample_rate;
         switch (config->format) {
@@ -3354,6 +3354,8 @@ static int adev_open_output_stream(struct audio_hw_device *dev,
             out->raw_61937_frame_size = 16;
             break;
         case TYPE_PCM:
+            out->config.period_size *= 2;
+            out->config.period_count *= 2;
             if (out->config.channels >= 6 || out->config.rate > 48000)
                 adev->hi_pcm_mode = true;
             break;
